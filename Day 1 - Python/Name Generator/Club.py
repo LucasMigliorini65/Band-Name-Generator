289ac3cd54f81b0ce5e7088a984c006e19801954
João Lucas Migliorini <110692154+LucasMigliorini65@users.noreply.github.com>
2024-12-19 18:49:07 -0300

    Add files via upload
    I'm learning Python and this was my learning from day 1, today I'm developing a simple project to generate a club name.
    #100DaysOfLearningPythonChallenge

---
+++ Day 1 - Python/Name Generator/Club.py
@@ -0,0 +1,6 @@
+#This project creates a new club name based in the city of you grew up and your favorite animal
+print("Hello this is the club name generator! :)")
+city = input("What's the name of the city you grew up in?\n")
+animal = input("What is the plural name of your favorite animal\n?")
+
+print("Your club name could be: " + city + " " + animal + " FC") 
